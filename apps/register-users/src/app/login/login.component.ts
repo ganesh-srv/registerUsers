ffb4fd5bca3c6c325fab5b16556ee3e809835598
unknown <ganeshsrv.sarakadam@gmail.com>
2021-07-01 18:53:39 +0530

    login pilot changes

--- apps/register-users/src/app/login/login.component.ts
+++ apps/register-users/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 
-import { Input, Component,OnInit, Output, EventEmitter } from '@angular/core';
+import { Input, Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -11,10 +11,13 @@ export class LoginComponent implements OnInit {
   public login: boolean | undefined;
   constructor() {
     this.login = true;
-   }
-
-  ngOnInit(): void {
-    
   }
 
+  ngOnInit(): void {
+
+  }
+
+  flicker() {
+    this.login = !this.login;
+  }
 }
