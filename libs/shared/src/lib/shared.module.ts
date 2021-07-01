23f80e8b0a45462ee416436665bbf73fcb9c6662
unknown <ganeshsrv.sarakadam@gmail.com>
2021-07-01 22:28:28 +0530

    created logout menu sticked onto the header

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatIconModule],
   declarations: [
     HeaderComponent,
     FooterComponent
